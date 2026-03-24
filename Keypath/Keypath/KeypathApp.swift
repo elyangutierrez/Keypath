367afa7ef4aadb716df51d8bb88ea67864dba7cf
Elyan Gutierrez <142635649+elyangutierrez@users.noreply.github.com>
2026-03-24 18:09:29 -0500

    hello repo

---
+++ Keypath/Keypath/KeypathApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeypathApp.swift
+//  Keypath
+//
+//  Created by Elyan Gutierrez on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct KeypathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Keypath/KeypathTests/KeypathTests.swift
@@ -0,0 +1,17 @@
+//
+//  KeypathTests.swift
+//  KeypathTests
+//
+//  Created by Elyan Gutierrez on 3/24/26.
+//
+
+import Testing
+@testable import Keypath
+
+struct KeypathTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
