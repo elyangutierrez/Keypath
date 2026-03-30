b0f27b99509132c648fec1d13f452e4257dcdb4f
Elyan Gutierrez <142635649+elyangutierrez@users.noreply.github.com>
2026-03-30 16:50:18 -0500

    can now move to and from apps

--- Keypath/Keypath/Core/IndividualPath/Model/Keypath.swift
+++ Keypath/Keypath/Core/IndividualPath/Model/Keypath.swift
@@ -13,10 +13,43 @@ class Keypath: Identifiable, Hashable, Comparable {
     var id = UUID()
     var application: NSRunningApplication
     var keybind: Keybind?
+    var isFrontmost: Bool = false
     
     init(application: NSRunningApplication) {
         self.application = application
     }
+    
+    func moveToApp() {
+        application.activate()
+        
+        isFrontmost = true
+        
+        if let appURL = application.bundleURL {
+            let config = NSWorkspace.OpenConfiguration()
+            
+            NSWorkspace.shared.open(appURL, configuration: config) { _, error in
+                if let error = error {
+                    print("Activation Error: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    func moveFromApp() {
+            let appElement = AXUIElementCreateApplication(application.processIdentifier)
+            var windowsValue: CFTypeRef?
+            
+            let result = AXUIElementCopyAttributeValue(appElement, kAXWindowsAttribute as CFString, &windowsValue)
+            
+            if result == .success, let windows = windowsValue as? [AXUIElement] {
+                for window in windows {
+                    AXUIElementSetAttributeValue(window, kAXMinimizedAttribute as CFString, true as CFTypeRef)
+                }
+                isFrontmost = false
+            } else {
+                print("Could not find windows to minimize, or app does not support it.")
+            }
+        }
     
     func hash(into hasher: inout Hasher) {
         hasher.combine(id)
